15a117f200095e0a25cf9296a21810af9af4f845
zach.whale <zach.whale@gmail.com>
2018-02-02 22:25:20 -0500

    test commit

--- src/dataaccesslayer/DataAccessLayer.java
+++ src/dataaccesslayer/DataAccessLayer.java
@@ -5,6 +5,11 @@
  */
 package dataaccesslayer;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+import java.sql.*;
+
 /**
  *
  * @author Zach
@@ -16,6 +21,31 @@ public class DataAccessLayer {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Properties prop = new Properties();
+        InputStream input = null;
+        Connection conn = null;
+        
+        try
+        {
+            input = new FileInputStream("C:\\Users\\Zach\\Documents\\NetBeansProjects\\DataAccessLayer\\src\\properties\\database_connection.properties");
+            
+            prop.load(input);
+            
+            String username = prop.getProperty("username");
+            String password = prop.getProperty("password");
+            String hostname = prop.getProperty("hostname");
+            String connectionString = "jdbc:mysql://localhost:3306/test_database";
+            
+            conn = DriverManager.getConnection(connectionString, username, password);
+            System.out.println("connected");
+            
+        } catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+        
     }
     
 }
